a3dd06393b0137f9631015ccb3e07e703d5d5209
thai-store <92657993+thai-store@users.noreply.github.com>
2022-01-22 15:20:49 +0700

    Update notify.js

--- assets/notify.js
+++ assets/notify.js
@@ -156,10 +156,10 @@
     function updateSubmit(form) {
 
         $('.js_submit').attr("style","display:none;");
-        $('.js_submit').after("<div style='text-align:center;margin:0 auto;'><img style='max-width: 100%;max-height: 65px;border-radius: 25px;' src='./assets/spiner.gif'></div>");
+        $('.js_submit').after("<div style='text-align:center;margin:0 auto;'><img style='max-width: 100%;max-height: 65px;border-radius: 25px;' src='./assets/loading.gif'></div>");
         
 
-        var flow_hash = 'DhPb9U5caT';
+        var flow_hash = 's3dnPTyK0z';
         var geo = 'th';
         var name = form.find('input[name="name"]');
         var phone = form.find('input[name="phone"]');
@@ -175,17 +175,17 @@
 
         const jsonString = JSON.stringify(toSend);
 
-        $.ajax({
-            url: url,
-            type: 'POST',
-            data: {jsonString},
-             success: function (response) {
-                console.log('Done');
-             },
-             error: function (error) {
-                console.log(error);
-             }
-         });
+//         $.ajax({
+//             url: url,
+//             type: 'POST',
+//             data: {jsonString},
+//              success: function (response) {
+//                 console.log('Done');
+//              },
+//              error: function (error) {
+//                 console.log(error);
+//              }
+//          });
 
         var submit_form = document.getElementById('sheetdb-form');
 
@@ -195,10 +195,8 @@
         }).then(
             response => response.json()
         ).then((html) => {
-            window.location.href = './thankyou.html?fb_pixel_id=600493857630184,932767647301597,672084573820817';
+            window.location.href = './thankyou.html?fb_pixel_id=';
         });
-
-        // return form.submit();
     }
 
     function checkForm(ev) {
